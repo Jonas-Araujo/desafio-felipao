b2e5185affda794baada2f2f87118a351c06cf78
Jonas Araujo <jonas_a_lima@hotmail.com>
2024-05-07 22:55:46 -0300

    desafio

---
+++ src/desafio.js
@@ -0,0 +1,23 @@
+let heroName = "Timão"
+let xpHero = 100001
+
+let nivel;
+if (xpHero <= 1000) {
+    nivel = "Ferro"
+} else if (xpHero >= 1001 && xpHero <= 2000) {
+    nivel = "Bronze"
+} else if (xpHero >= 2001 && xpHero <= 5000) {
+    nivel = "Prata"
+} else if (xpHero >= 5001 && xpHero <= 7000) {
+    nivel = "Ouro"
+} else if (xpHero >= 7001 && xpHero <= 8000) {
+    nivel = "Platina"
+} else if (xpHero >= 8001 && xpHero <= 9000) {
+    nivel = "Ascendente"
+} else if (xpHero >= 9001 && xpHero <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
+}
+
+console.log(`O Herói de nome ${heroName} está no nível de ${nivel}`);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let nome = "Jonas";
-console.log("Olá");
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Digite o nome do Herói: ", function (heroName) {
+    rl.question("Digite a quantidade de XP do Herói: ", function (xpHero) {
+        xpHero = parseInt(xpHero);
+
+        let nivelHero;
+        if (xpHero <= 1000) {
+            nivelHero = "Ferro";
+        } else if (xpHero >= 1001 && xpHero <= 2000) {
+            nivelHero = "Bronze";
+        } else if (xpHero >= 2001 && xpHero <= 5000) {
+            nivelHero = "Prata";
+        } else if (xpHero >= 5001 && xpHero <= 7000) {
+            nivelHero = "Ouro";
+        } else if (xpHero >= 7001 && xpHero <= 8000) {
+            nivelHero = "Platina";
+        } else if (xpHero >= 8001 && xpHero <= 9000) {
+            nivelHero = "Ascendente";
+        } else if (xpHero >= 9001 && xpHero <= 10000) {
+            nivelHero = "Imortal";
+        } else {
+            nivelHero = "Radiante";
+        }
+
+        console.log(`O Herói de nome ${heroName} está no nível de ${nivelHero}`);
+
+        rl.close();
+    });
+});
